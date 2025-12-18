0727b3989039da043a82b71244bf6fa430ff32f0
bala-amypo <bala@amypo.in>
2025-12-18 16:44:35 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,58 +1,67 @@
 package com.example.demo.model;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
 import java.time.LocalDateTime;
 
 
-// public class User{
-//     private Long id;
-//     private String name;
-//     private String email;
-//     private String password;
-//     private String role;
-//     private LocalDateTime createdAt;
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private LocalDateTime createdAt;
 
-//     public User(Long id,String name, String email,String password, String role, String createdAt){
-            @Id
-            @GeneratedValue(strategy = GenerationType.IDENTITY)
-//         this.id=id;
-//         this.name=name;
-//         this.email=email;
-//         this.password=password;
-//         this.role=role;
-//         this.createdAt=createdAt;
-//     }
+    public User(Long id,String name, String email,String password, String role, String createdAt){
+           
+        this.id=id;
+        @Size()
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.role=role;
+        this.createdAt=createdAt;
+    }
 
-//     public Long getId(){
-//         return id;
-//     }
-//     public void setId(Long id){
-//         this.id=id;
-//     }
-//      public String getName(){
-//         return name;
-//     }
-//     public void setName(String name){
-//         this.name=name;
-//     }
-//      public String getEmail(){
-//         return email;
-//     }
-//     public void setEmail(String email){
-//         this.email=email;
-//     }
-//      public String getPassword(){
-//         return password;
-//     }
-//     public void setPassword(String password){
-//         this.password=password;
-//     }
-//     public LocalDateTime getCreatedAt(){
-//         return createdAt;
-//     }
-//     public void setCreatedAt(LocalDateTime createdAt){
-//         this.createdAt=createdAt;
-//     }
-// }
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+     public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+     public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+     public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public  String getRole(){
+        return role;
+    }
+    public void setRole(String role){
+      this.role=role;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt=createdAt;
+    }
+}
 
 
 
