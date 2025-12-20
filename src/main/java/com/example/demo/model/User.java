fe997a4cea7c663ea04069a55a6d1db725dc8766
bala-amypo <bala@amypo.in>
2025-12-20 16:35:26 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,57 +1,47 @@
-package com.example.demo.model;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Positive;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User{
+public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotNull
-    @Size(min= 2,max = 10, message="must be 2 to 10 character")
+    @Size(min = 2, max = 10, message = "must be 2 to 10 character")
     private String name;
-    @Email(message="Email is not valid")
+
+    @Email(message = "Email is not valid")
     private String email;
-    @Size(min= 2,max = 10, message="must be 2 to 10 character")
-    @NotNull(message=" Password is mandatory")
+
+    @Size(min = 2, max = 10, message = "must be 2 to 10 character")
+    @NotNull(message = "Password is mandatory")
     private String password;
+
     private String role;
+
     private LocalDateTime createdAt;
 
     
-   
+    @PrePersist
+    public void setCreatedAt() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-       
-         
-
-
-        
-
-
-
