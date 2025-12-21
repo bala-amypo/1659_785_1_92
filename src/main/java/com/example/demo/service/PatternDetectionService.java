5885ece73bfdf012d40fd1ae8a515c2ba3157384
bala-amypo <bala@amypo.in>
2025-12-21 13:16:26 +0000

    User commit

--- src/main/java/com/example/demo/service/PatternDetectionService.java
+++ src/main/java/com/example/demo/service/PatternDetectionService.java
@@ -1,10 +1,14 @@
 package com.example.demo.service;
 
-
+import com.example.demo.model.;
+import java.util.List;
 
 @Service
 public interface PatternDetectionService{
        
-       PatternDetectionResult detectPattern()
+       PatternDetectionResult detectPattern(Long zoneId);
+
+       List<PatternDetectionResult>getResultsByZone(Long zoneId);
+
 
 }
