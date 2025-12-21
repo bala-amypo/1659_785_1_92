7704879ece55a1022cc4a0215cdf2517457e946a
bala-amypo <bala@amypo.in>
2025-12-21 05:29:20 +0000

    User commit

--- src/main/java/com/example/demo/model/CrimeReport.java
+++ src/main/java/com/example/demo/model/CrimeReport.java
@@ -1,15 +1,11 @@
 package com.example.demo.model;
-
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,27 +15,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CrimeReport {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotNull(message = "Crime type is mandatory")
     private String crimeType;
-
     @NotNull(message = "Description is mandatory")
     private String description;
-
     @NotNull(message = "Latitude is mandatory")
-    
     private Double latitude;
-
     @NotNull(message = "Longitude is mandatory")
-
     private Double longitude;
-
     @NotNull(message = "Occurred time is mandatory")
     @PastOrPresent(message = "Occurred time cannot be in the future")
-    
     private LocalDateTime occurredAt;
 }
