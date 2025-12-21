f4e72a55b89ae42e20b47cdd41af5b7277a53439
bala-amypo <bala@amypo.in>
2025-12-21 02:36:08 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -3,3 +3,87 @@ pacakge com.example.demo.service.Impl;
 
 
 
+
+
+@Service
+ public class UserServiceImpl implements UserService{ 
+          @Autowired UserRepository  service;
+          
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package com.example.demo.service.Impl;
+// import org.springframework.beans.factory.annotation.Autowired;
+// // import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.stereotype.Service;
+// import com.example.demo.repository.Studentrepo;
+// import com.example.demo.entity.Studententity;
+// import com.example.demo.service.Studentservice;
+// import java.util.List;
+// @Service  
+// public class StudentserviceImpl implements Studentservice{   //splited as 2 layers here
+//             @Autowired Studentrepo student; 
+//             //save()-insert,update
+//             //findall(),findById(),deleteById(),existById()
+//             @Override
+//             public  Studententity postData(Studententity stu){
+//                return student.save(stu);
+//             }
+//             @Override
+//             public   List<Studententity>getAllData(){
+//                return student.findAll();
+//             }
+//             @Override
+//              public String delete(int id){
+//                  student.deleteById(id);  // if add return ,it will give error
+//                  return "Deleted Successfully";
+//              }
+//             @Override
+//             public  Studententity getData(int id){          //return type:Studententity ,gives error -> so add orElse(null)
+//                return student.findById(id).orElse(null);
+//             }
+//             @Override
+//             public  Studententity updatedata(int id,Studententity entity){
+//                if(student.existsById(id)){
+//                   entity.setId(id);
+//                   return student.save(entity);
+//                }
+//                return null;
+//             }
+
+// }
