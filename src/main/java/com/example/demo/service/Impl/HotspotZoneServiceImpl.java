1e2c87d5ac707237036428245a3607e650f50d65
bala-amypo <bala@amypo.in>
2025-12-21 06:53:16 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/HotspotZoneServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/HotspotZoneServiceImpl.java
@@ -3,22 +3,22 @@ package com.example.demo.service.Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.service.CrimeReportService;
-import com.example.demo.repository.CrimeReportRepository;
+import com.example.demo.service.HotspotZoneService;
+import com.example.demo.repository.Repository;
 import com.example.demo.model.CrimeReport;
 
 import java.util.List;
 
 @Service
-public class HotspotZoneServiceImpl implements HService{
-    @Autowired CrimeReportRepository  crime;
+public class HotspotZoneServiceImpl implements HotspotZoneService{
+    @Autowired HotspotZoneRepository  hotspot;
 
     @Override 
-    public CrimeReport addReport(CrimeReport report){
-        return crime.save(report);
+    public HotspotZone addZone(HotspotZone zone){
+        return hotspot.save(zone);
     }
     @Override
-    public List<CrimeReport>getAllReports(){
-        return crime.findAll();
+    public List<HotspotZone>getAllZones(){
+        return hotspot.findAll();
     }
 }
