88510b12e9a0c1ba239c36307a8ce5949c51f7be
bala-amypo <bala@amypo.in>
2025-12-21 04:36:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/CrimeReportController.java
+++ src/main/java/com/example/demo/controller/CrimeReportController.java
@@ -1,7 +1,18 @@
 package com.example.demo.controller;
 
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import com.example.demo.model.CrimeReport;
+import com.example.demo.service.CrimeReportService;
 
 @RestController
 public class CrimeReportController {
@@ -9,11 +20,11 @@ public class CrimeReportController {
     @Autowired
     private CrimeReportService crimeservice;
 
-    @PostMapping("/")
-    public CrimeReport valReport(@RequestBody CrimeReport report){
+    @PostMapping("/post")
+    public CrimeReport valReport(@Valid @RequestBody CrimeReport report){
         return crimeservice.addReport(report);
     }
-    @GetMapping("/")
+    @GetMapping("/get")
     public List<CrimeReport> getval(){
         return crimeservice.getAllReports();
     }
