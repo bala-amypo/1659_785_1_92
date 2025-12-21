cea0ab657a710356e9493db8cfc71a5d42b734ec
bala-amypo <bala@amypo.in>
2025-12-21 07:17:13 +0000

    User commit

--- src/main/java/com/example/demo/repository/HotspotZoneRepository.java
+++ src/main/java/com/example/demo/repository/HotspotZoneRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import  com.example.demo.model.HotspotZone;
 
 public interface HotspotZoneRepository extends JpaRepository<HotspotZone,Long>{
