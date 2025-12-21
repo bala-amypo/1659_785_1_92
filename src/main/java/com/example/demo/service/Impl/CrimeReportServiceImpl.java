c5e954b13d5a933f519c0681d0151dcda214017c
bala-amypo <bala@amypo.in>
2025-12-21 04:18:03 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/CrimeReportServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/CrimeReportServiceImpl.java
@@ -1,5 +1,11 @@
 package com.example.demo.service.Impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.service.CrimeReportService;
+import com.example.demo.repository.Repository;
+import com.example.demo.model.User;
 
 
 
@@ -8,4 +14,11 @@ public class CrimeReportServiceImpl implements CrimeReportService{
     @Autowired CrimeReportRepository  crime;
 
     @Override 
+    public CrimeReport addReport(CrimeReport report){
+        return crime.save(report);
+    }
+    @Override
+    public List<CrimeReport>getAllReports(){
+        return crime.findAll();
+    }
 }
