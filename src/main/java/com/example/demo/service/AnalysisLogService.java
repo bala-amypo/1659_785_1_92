1d0df3a36b9c723aded85c64f85513e763e02204
bala-amypo <bala@amypo.in>
2025-12-21 14:01:26 +0000

    User commit

--- src/main/java/com/example/demo/service/AnalysisLogService.java
+++ src/main/java/com/example/demo/service/AnalysisLogService.java
@@ -3,5 +3,9 @@ package com.example.demo.service;
 
 
 public interface AnalysisLogService{
-    
+
+
+    AnalysisLog addLog(Long zoneId,String message);
+
+    List<AnalysisLog>getLogsByZone(Long zoneI)
 }
