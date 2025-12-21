fb1ce3eaf60bce45dba1f07fe46f9ff398560170
bala-amypo <bala@amypo.in>
2025-12-21 06:33:15 +0000

    User commit

--- src/main/java/com/example/demo/service/HotspotZoneService.java
+++ src/main/java/com/example/demo/service/HotspotZoneService.java
@@ -1,11 +1,14 @@
 package com.example.demo.service;
 
+import com.example.demo.model.HotspotZone;
+import java.util.List;
 
 
 
-
-public interface Hotsp
+public interface HotspotZoneService{
 
       HotspotZone addZone(HotspotZone zone);
 
       List<HotspotZone>getAllZones();
+
+}
