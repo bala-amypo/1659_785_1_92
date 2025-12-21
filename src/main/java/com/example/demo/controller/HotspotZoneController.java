a010dc3714e1f8243abb6e51bcd3e45a4c284d38
bala-amypo <bala@amypo.in>
2025-12-21 12:28:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/HotspotZoneController.java
+++ src/main/java/com/example/demo/controller/HotspotZoneController.java
@@ -1,16 +1,11 @@
 package com.example.demo.controller;
-
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import jakarta.validation.Valid;
-
 import com.example.demo.model.HotspotZone;
 import com.example.demo.service.HotspotZoneService;
 
