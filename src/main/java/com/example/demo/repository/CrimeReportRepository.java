2c7a1653d2d8f4daa38a714d5cdfce11a4bf8597
bala-amypo <bala@amypo.in>
2025-12-23 05:51:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/CrimeReportRepository.java
+++ src/main/java/com/example/demo/repository/CrimeReportRepository.java
@@ -7,7 +7,7 @@ import com.example.demo.model.CrimeReport;
 
 @Repository
 public interface CrimeReportRepository extends JpaRepository<CrimeReport,Long>{
-     List<CrimeReport> findByLatLongRange(
-            double minLat, double maxLat,
-            double minLong, double maxLong);
+     // List<CrimeReport> findByLatLongRange(
+     //        double minLat, double maxLat,
+     //        double minLong, double maxLong);
 }
