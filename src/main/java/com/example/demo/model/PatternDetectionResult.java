20460f1690b0864bd114a93c531e8e17c1f424b4
bala-amypo <bala@amypo.in>
2025-12-23 16:46:38 +0000

    User commit

--- src/main/java/com/example/demo/model/PatternDetectionResult.java
+++ src/main/java/com/example/demo/model/PatternDetectionResult.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.Id;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
