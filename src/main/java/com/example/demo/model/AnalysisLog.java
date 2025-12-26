b08d2289ede94780d9c880d5e34171f095309189
bala-amypo <bala@amypo.in>
2025-12-26 19:02:16 +0000

    User commit

--- src/main/java/com/example/demo/model/AnalysisLog.java
+++ src/main/java/com/example/demo/model/AnalysisLog.java
@@ -1,41 +1,76 @@
-package com.example.demo.model;
+// package com.example.demo.model;
 
-import java.time.LocalDateTime;
+// import java.time.LocalDateTime;
+
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.PrePersist;
+// import jakarta.validation.constraints.NotNull;
+
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+
+// @Entity
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class AnalysisLog {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     @NotNull
+//     private String message;
+
+//     private LocalDateTime loggedAt;
+
+//      @ManyToOne
+//     @JoinColumn(name = "zone_id")
+//     private HotspotZone zone;
+
+//     @PrePersist
+//     private void setLoggedAt() {
+//         this.loggedAt = LocalDateTime.now();
+//     }
+// }
+
+
+package com.example.demo.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
-import jakarta.validation.constraints.NotNull;
-
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
-@Entity
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "analysis_logs")
 public class AnalysisLog {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotNull
     private String message;
-
     private LocalDateTime loggedAt;
-
-     @ManyToOne
-    @JoinColumn(name = "zone_id")
+    @ManyToOne
     private HotspotZone zone;
 
     @PrePersist
-    private void setLoggedAt() {
+    protected void onLog() {
         this.loggedAt = LocalDateTime.now();
     }
 }
